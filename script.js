c928139cd769c59efd36d697b1d8a9542ec5c2d3
PRaymundoCuti <praymundocuti@rrc.ca>
2026-03-07 17:49:51 -0600

    feat: js functions implementation

---
+++ script.js
@@ -0,0 +1,40 @@
+function isNotEmpty(input){
+    if(input===""){
+        return false;
+    }else{
+        return true;
+    }      
+}
+function isValidEmail(email){
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email)
+}
+function hasCheckedPoint(fieldset){
+    const inputList =[...fieldset.querySelectorAll("input")];
+    for(let element of inputList ){
+        if (element.checked){
+            return true;
+        }
+    }
+    return false;
+}
+
+function isSelected(select){
+    return isNotEmpty(select.value)
+}
+function isValidPhoneNumber(number){
+    const PhoneNumberPattern = /^[0-9]{10}$/;
+    return PhoneNumberPattern.test(number)
+}
+function isInFuture(date){
+    const today = new Date();
+    today.setHours(0,0,0,0)
+
+    const inputDate =new Date(date);
+    inputDate.setHours(0,0,0,0)
+
+    return today<inputDate
+}
+function numberValidate(number){
+    return isNotEmpty(number) && number.min<number && number.max<number
+}
